d7d476b56c6d4f2305bf2c69c7cb50c2dc07f540
Ajulay <11v09v@gmail.com>
2018-07-05 13:20:34 +0300

    step0

---
+++ common/src/main/java/com/cloud/storage/common/TransmitClass.java
@@ -0,0 +1,18 @@
+package com.cloud.storage.common;
+
+import java.io.Serializable;
+
+public class TransmitClass implements Serializable {
+    String fileName;
+    char[] buffer;
+    int part;
+
+    public TransmitClass(String fileName, char[] buffer, int part) {
+        this.fileName = fileName;
+        this.buffer = buffer;
+        this.part = part;
+    }
+
+
+
+}
--- server/src/main/java/com/cloud/storage/server/ServerMain.java
+++ server/src/main/java/com/cloud/storage/server/ServerMain.java
@@ -1,6 +1,6 @@
 package com.cloud.storage.server;
 
-public class ServerMainClass {
+public class ServerMain {
     public static void main(String[] args) {
 
     }
